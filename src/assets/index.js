5128b6aa11cbe5825c0940b51fbe632bbfed81fc
Chemini <cheminimallikarachchi@gmail.com>
2024-07-22 16:27:00 +0530

    Updated project section

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,12 @@ import bannerImg from "./bannerImg.png";
 import workImgTwo from "./work/workImgTwo.jpg"
 import workImgThree from "./work/workImgThree.jpg"
 import workImgFive from "./work/workImgFive.jpg"
+import ImgOne from "./work/ImgOne.jpg"
 
 export { 
     bannerImg,
     workImgTwo,
     workImgThree,
+    ImgOne,
     workImgFive
 };
